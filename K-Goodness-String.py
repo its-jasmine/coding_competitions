19cbe238a245d65c09ff63281fc3532d7b9cd1c7
its-jasmine <97357122+its-jasmine@users.noreply.github.com>
2022-01-10 14:32:01 -0500

    Removing un-necessary attribute, added commenting

--- K-Goodness-String.py
+++ K-Goodness-String.py
@@ -6,27 +6,29 @@ class Adas_String:
         self.string = string
         self.n = n
         self.k = k
-        self.good_i = []
         self.goodness_score = self.goodness()
-
+    
+    # Returns goodness score of string
     def goodness(self):
         score = 0
         for i in range(int(self.n/2)):
-            if (self.string[i] != self.string[self.n - i - 1]):
+            if (self.string[i] != self.string[self.n - i - 1]): 
                 score += 1
-                self.good_i.append(i)
         return score
+    
+    # Determines minimum number of operations to acheive desired goodness score of k
     def min_op(self):
-        if self.goodness_score == k:
+        if self.goodness_score == k: 
             return 0
         elif self.goodness_score < k:
             return k - self.goodness_score
         elif self.goodness_score > k:
             return self.goodness_score - k
+        
+    # Responsible for output in requested format    
     def result(self, i):
         min = self.min_op()
         print(f"Case #{i}: {min}")
-
 
 num_tests = int(input())
 for i in range(num_tests):
@@ -36,9 +38,3 @@ for i in range(num_tests):
 
 
 
-
-'''
-case 1: string already has goodness score of k
-case 2: string has goodness score < k
-case 3: string has goodness score > k
-'''
